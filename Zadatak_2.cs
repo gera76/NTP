d0747a52c27b22e0ef3b5d3737d510b62bded1ef
gera76 <gerasimovicdimitrije7@gmail.com>
2024-11-05 22:15:12 +0100

    Kontrolni

---
+++ Zadatak_2.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi velicinu niza:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            int[] niz = new int[n];
+
+            while(i < n)
+            {
+                Console.WriteLine($"Unesi {i}-ti element:");
+                int el = Convert.ToInt32(Console.ReadLine());
+                niz[i] = el;
+                i++;
+            }
+
+            SelectionSort(niz, n);
+            i = 0;
+
+            while (i < n)
+            {
+                Console.Write(niz[i]);
+                Console.Write(", ");
+
+                i++;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Unesite element koji trazite:");
+            int trazeni = Convert.ToInt32(Console.ReadLine());
+
+            int nadjeno = binarna_pretraga(niz, 0, n-1, trazeni);
+
+            if (nadjeno != -1)
+                Console.WriteLine($"Trazeni element {trazeni} je na poziciji {nadjeno}.");
+            else
+                Console.WriteLine("Trazeni element ne postoji");
+
+            Console.ReadLine();
+        }
+
+
+        //pazite sa binarnom pretragom ovde jer sad trazite u nizu sa nerastucim poretkom!
+        private static int binarna_pretraga(int[] a, int l, int d, int x)
+        {
+            int s;
+            if (l > d)
+            {
+                return -1;
+            }
+            s = l + (d - l) / 2;
+            if (x == a[s])
+            {
+                return s;
+            }
+            if (x < a[s])
+            {
+                return binarna_pretraga(a, s + 1, d, x);
+            }
+            else /* if (x > a[s]) */
+            {
+                return binarna_pretraga(a, l, s - 1, x);
+            }
+        }
+
+        private static void SelectionSort(int[] niz, int n)
+        {
+            int i, j;
+            for (i = 0; i < n - 1; i++)
+            {
+                for (j = i + 1; j < n; j++)
+                {
+                    if (niz[i] < niz[j])
+                    {
+                        razmena(ref niz[i], ref niz[j]);
+                    }
+                }
+            }
+        }
+
+        //verzija razmene sa referenciranjem
+        //umesto ovoga mozete koristiti klasican metod bez referenci gde saljemo ceo niz i vrednosti indeksa koje
+        //zelimo razmeniti u nizu (korisceno u Zadatak 1)
+        private static void razmena(ref int a, ref int b)
+        {
+            int pom = a;
+            a = b;
+            b = pom;
+        }
+    }
+}
---
+++ algebarski alogirtmi/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        long n = long.Parse(Console.ReadLine());
+        long a = long.Parse(Console.ReadLine());
+        long b = long.Parse(Console.ReadLine());
+
+        // Provera primalnosti naivni
+        provera_primalnosti_naivni(n);
+
+        // Provera primalnosti optimizovan
+        provera_primalnosti_optimizovan(n);
+
+        // Faktorizacija
+        faktorizacija(n);
+
+        // NZD iterativni
+        long nzd_I = nzdIterativni(a, b);
+        Console.WriteLine($"NZD iterativni: {nzd_I}");
+
+        // NZD rekurzivni
+        long nzd_R = nzd_rekurzivni(a, b);
+        Console.WriteLine($"NZD rekurzivni: {nzd_R}");
+
+        long nzs_I = nzsIterativni (a, b);
+        Console.WriteLine($"NZS interativni: {nzd_I}");
+
+        long nzs_R =nzd_rekurzivni (a, b);
+
+        Console.WriteLine($"NZS rekurzivni: {nzs_R}");
+
+        Console.ReadLine();
+    }
+
+    // Provera primalnosti naivni
+    static void provera_primalnosti_naivni(long n)
+    {
+        bool prost;
+        if (n == 1)
+        {
+            prost = false; // broj 1 nije prost
+        }
+           
+        else
+        {
+            prost = true;
+            for (long i = 2; i < n; i++)
+                if (n % i == 0)
+                {
+                    prost = false;
+                    i = n; // izlaz iz petlje
+                }
+        }
+        Console.WriteLine(prost ? "DA" : "NE");
+    }
+
+    // Provera primalnosti optimizovan
+    static void provera_primalnosti_optimizovan(long n)
+    {
+        long a = (long)Math.Ceiling(Math.Sqrt(n));
+        bool prost;
+        if (n == 1)
+        {
+            prost = false;
+        }
+        else if (n == 2)
+        {
+            prost = true;
+        }
+        else
+        {
+            prost = true;
+            for (long i = 2; i <= a; i++)
+                if (n % i == 0)
+                {
+                    prost = false;
+                    break;
+                }
+        }
+        Console.WriteLine(prost ? "DA" : "NE");
+    }
+
+    // Faktorizacija
+    static void faktorizacija(long n)
+    {
+        Stopwatch b = new Stopwatch();
+        b.Start();
+        long a = (long)Math.Ceiling(Math.Sqrt(n));
+        long i = 2;
+        while (i <= a)
+        {
+            if (n % i == 0)
+            {
+                Console.WriteLine(i);
+                n = n / i;
+                a = (long)Math.Ceiling(Math.Sqrt(n));
+            }
+            else
+            {
+                i++;
+            }
+        }
+        if (n > 1)
+        {
+            Console.WriteLine(n);
+        }
+        b.Stop();
+        Console.WriteLine($"Vreme: {b.ElapsedMilliseconds * 0.001} sekundi");
+    }
+
+    // NZD iterativni
+    static long nzdIterativni(long a, long b)
+    {
+        while (b != 0)
+        {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // NZD rekurzivni
+    static long nzd_rekurzivni(long a, long b)
+    {
+        if (b == 0)
+        {
+            return a;
+        }
+        else
+        {
+            return nzd_rekurzivni(b, a % b);
+        }
+    }
+
+
+    static long nzsIterativni(long a, long b)
+    {
+       return (a*b)/nzdIterativni(a, b);
+    }
+    static long nzs_rekurzivni(long a, long b)
+    {
+        return (a * b) / nzd_rekurzivni (a, b);
+    }
+}
